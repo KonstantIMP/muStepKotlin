91b689d9e253956cf0d99fd7ce985f5b261b3e4b
KonstantIMP <mihedovkos@gmail.com>
2022-06-11 11:27:48 +0300

    Add Floors caching

--- app/src/main/java/org/kimp/mustep/utils/AppCache.kt
+++ app/src/main/java/org/kimp/mustep/utils/AppCache.kt
@@ -3,14 +3,18 @@ package org.kimp.mustep.utils
 import android.content.Context
 import android.net.Uri
 import android.util.Log
+import android.util.LruCache
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.AmazonS3URI
 import java.io.File
+import org.kimp.mustep.domain.Floor
 
 object AppCache {
     const val IMAGE_CACHE_SIZE: Long = 32 * 1024 * 1024
+    private const val DATA_CACHE_SIZE: Int = 16 * 1024 * 1024
+
+    private var floorCache: LruCache<String, List<Floor>> = LruCache(DATA_CACHE_SIZE)
 
     fun getCacheSupportUri(relativePath: String, context: Context): Uri {
         Log.i("Cache", String.format("Requested: %s", relativePath))
@@ -27,4 +31,10 @@ object AppCache {
             s3Client.getResourceUrl("mustep", relativePath)
         )
     }
+
+    fun putFloors(uid: String, floors: List<Floor>) {
+        floorCache.put(uid, floors)
+    }
+
+    fun getFloors(uid: String) : List<Floor>? = floorCache.get(uid)
 }
