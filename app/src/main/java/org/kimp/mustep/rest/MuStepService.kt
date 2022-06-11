784fc9389b57dbab58b4176ad37bae0abd58823b
KonstantIMP <mihedovkos@gmail.com>
2022-06-11 11:29:27 +0300

    Floors getting method

--- app/src/main/java/org/kimp/mustep/rest/MuStepService.kt
+++ app/src/main/java/org/kimp/mustep/rest/MuStepService.kt
@@ -1,11 +1,16 @@
 package org.kimp.mustep.rest
 
+import org.kimp.mustep.domain.Floor
 import org.kimp.mustep.domain.University
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MuStepService {
     @GET("university/available")
     fun getAvailableUniversities(@Query("onlyOfficial") onlyOfficial: Boolean = false) : Call<List<University>>
+
+    @GET("university/{uni}/data")
+    fun getUniversityData(@Path("uni") uni: String) : Call<List<Floor>>
 }
